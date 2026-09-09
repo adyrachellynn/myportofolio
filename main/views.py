26db1e6d6be6e64c2837ce7ce749118be67cfb5e
adyrachellyn <adyra.rachellyn@ui.ac.id>
2026-09-09 22:25:00 +0700

    Finalize Tutorial 02: Experience page layout and responsive bento navbar

--- main/views.py
+++ main/views.py
@@ -2,23 +2,21 @@ from django.shortcuts import render
 
 from main.models import Experience
 
-
 def show_main(request):
     context = {
-        "name": "Adyra Rachellyn Arkossand",
-        "npm": "2506620620",
+        "name": "Adyra Rachellyn Arkossand",  
+        "npm": "2506620620",  
         "study_program": "S1 Sistem Informasi",
         "bio": (
-            "Mahasiswa Sistem Informasi Universitas Indonesia yang tertarik "
-            "pada data analytics dan product management"
+            "Mahasiswa Sistem Informasi Universitas Indonesia yang berfokus "
+            "pada Product Management dan Data Analytics."
         ),
     }
     return render(request, "index.html", context)
 
-
 def show_experience(request):
     context = {
-        "name": "Burhan",
+        "name": "Adyra",  # Pastikan sama dengan nama di show_main
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
