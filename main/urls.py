00aea5ff28a9451a19de1910bc66e1d7d32b1f0a
adyrachellyn <adyra.rachellyn@ui.ac.id>
2026-09-13 23:02:44 +0700

    feat(ui): implement education view, template, and responsive navbar routing

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-
-from main.views import show_main, show_experience
+from main.views import show_education, show_experience, show_main
 
 app_name = "main"
 
 urlpatterns = [
     path("", show_main, name="show_main"),
     path("experience/", show_experience, name="show_experience"),
+    path("education/", show_education, name="show_education"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from main.models import Experience
+from main.models import Experience, Education
 
 def show_main(request):
     context = {
@@ -20,3 +20,11 @@ def show_experience(request):
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
+
+def show_education(request):
+    education_list = Education.objects.all()
+    context = {
+        "name": "Adyra",
+        "education_list": education_list,
+    }
+    return render(request, "education.html", context)
